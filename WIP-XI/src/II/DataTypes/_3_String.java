dc5d7486f285e42139d1e496fb9cf958199e2a1d
pipe34150 <veerapat.ch@mail.kmutt.ac.th>
2019-05-30 11:37:29 +0700

    [ADD] New Project

---
+++ WIP-XI/src/II/DataTypes/_3_String.java
@@ -0,0 +1,29 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package II.DataTypes;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class _3_String {
+    public static void main(String[] args) {
+        String firstName = "Wippo";
+        String lastName = "Ontour";
+        String nickName = "Wip";
+        
+        System.out.println("firstName : " + firstName);
+        System.out.println("lastName : " + lastName);
+        System.out.println("nickName : " + nickName);
+        
+        System.out.println("Hi! my name is " + firstName + lastName + " (" + nickName + ").");
+        
+        boolean x = (10!=8);
+        boolean y = (10==8);
+        boolean z = x || y;
+        
+        System.out.println(z);
+    }
+}
---
+++ WIP-XI/src/VI/Input/_0_Scanner.java
@@ -0,0 +1,33 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package VI.Input;
+
+import java.util.Scanner;
+
+/**
+ * อย่าลืม!!!! IMPORT PACKAGE
+ */
+
+
+public class _0_Scanner {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter x : ");
+
+        int x = scanner.nextInt();
+        
+        System.out.print("Enter y : ");
+
+        int y = scanner.nextInt();
+        
+        System.out.println("X : " + x);
+        System.out.println("Y : " + y);
+        
+        System.out.println("X+Y : " + (x+y));
+        
+    }
+}
